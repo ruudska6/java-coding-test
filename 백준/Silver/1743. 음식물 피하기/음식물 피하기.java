25deaa246ebb868fe7b27b8661c015081ca39d6e
GYEONGNAM <feels19@naver.com>
2025-09-28 16:01:40 +0900

    [Silver I] Title: 음식물 피하기, Time: 140 ms, Memory: 16012 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
@@ -4,25 +4,25 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m, k;
+    static int n, m;
     static boolean[][] visited;
     static int[][] map;
     static int[] dr = {-1, 1, 0, 0};
     static int[] dc = {0, 0, -1, 1};
+    static int component = 0;
 
-    static int dfs(int x, int y) {
-        int component = 1;
+    static void dfs(int x, int y) {
+        component++;
         visited[x][y] = true;
         for (int i = 0; i < 4; i++) {
             int nr = x + dr[i];
             int nc = y + dc[i];
             if (nr > 0 && nc > 0 && nr <= n && nc <= m) {
                 if (map[nr][nc] == 1 && !visited[nr][nc]) {
-                    component += dfs(nr, nc);
+                     dfs(nr, nc);
                 }
             }
         }
-        return component;
     }
 
     public static void main(String[] args) throws Exception {
@@ -31,7 +31,7 @@ public class Main {
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
         visited = new boolean[n+1][m+1];
         map = new int[n+1][m+1];
@@ -47,8 +47,9 @@ public class Main {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
                 if (map[i][j] == 1) {
-                    int cnt = dfs(i, j);
-                    max = Math.max(max, cnt);
+                    dfs(i, j);
+                    max = Math.max(max, component);
+                    component = 0;
                 }
             }
         }
