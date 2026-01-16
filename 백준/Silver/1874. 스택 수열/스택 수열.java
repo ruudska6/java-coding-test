5d36c040330c36f96285252c26544de6cd20015a
GYEONGNAM <feels19@naver.com>
2026-01-17 00:07:39 +0900

    [Silver II] Title: 스택 수열, Time: 864 ms, Memory: 106916 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -2,41 +2,38 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
-	public static void main(String[] args) {
-		Stack<Integer> stack = new Stack<>();
-		Scanner sc = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
-		
 
-		int n = sc.nextInt();
-		int[] arr = new int[n];
-		
-		for(int i = 0; i < n; i++) {
-			arr[i] = sc.nextInt();
-		}
-		
-		int p = 0;
-		boolean flag = true;
-		for(int i = 1; i <= n; i++) {
-			stack.push(i);
-			sb.append("+").append("\n");
-			
-			
-			while(!stack.isEmpty() && stack.peek() == arr[p]) {
-					stack.pop();
-					p++;
-					sb.append("-").append("\n");
-			}
-		}
-		
-		if(stack.isEmpty()) {
-			System.out.println(sb.toString());
-		} else {
-			System.out.println("NO");
-		}
+    public static void main(String[] args) {
 
-	
-		
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
 
-	}
+        int num = 1;
+        
+        for (int i = 0; i < n; i++) {
+            int input = scanner.nextInt();
+
+            if (num <= input) {
+                while(num <= input) {
+                    stack.push(num++);
+                    sb.append("+").append("\n");
+                }
+
+                stack.pop();
+                sb.append("-").append("\n");
+            } else {
+                int top = stack.pop();
+                if (top == input) {
+                    sb.append("-").append("\n");
+                } else {
+                    System.out.println("NO");
+                    return;
+                }
+            }
+        }
+        
+        System.out.println(sb);
+    }
 }
