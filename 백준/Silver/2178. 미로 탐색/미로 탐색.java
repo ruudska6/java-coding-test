41c44884cce3a64134a77ee5d6cdf43603cd6969
GYEONGNAM <feels19@naver.com>
2026-01-25 19:00:59 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14640 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,65 +1,59 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n,m;
+
     static int[][] map;
     static boolean[][] visited;
+    static int n,m;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static int[][] distance;
 
-    static Queue<int[]> queue = new LinkedList<>();
+    static void bfs(int i, int j) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{i, j});
+        visited[i][j] = true;
 
-    static void bfs(int x, int y) {
-        queue.add(new int[] {x, y});
-        visited[x][y] = true;
-        distance[x][y] = 1;
+        while (!q.isEmpty()) {
+            int[] now = q.poll();
+            int x =  now[0];
+            int y = now[1];
 
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            int curX = cur[0];
-            int curY = cur[1];
+            for (int k = 0; k < 4; k++) {
+                int nx = dx[k] + x;
+                int ny = dy[k] + y;
 
-            for (int i = 0; i < 4; i++) {
-                int nx = curX + dx[i];
-                int ny = curY + dy[i];
-
-                if (nx >= 0 && ny >=0 && nx < n && ny < m && map[nx][ny] == 1 && !visited[nx][ny]) {
-                    queue.add(new int[] {nx, ny});
+                if (nx >= 1 && ny >= 1 && nx <= n && ny <= m && map[nx][ny] == 1 && !visited[nx][ny]) {
                     visited[nx][ny] = true;
-                    distance[nx][ny] = distance[curX][curY] + 1;
+                    q.add(new int[]{nx, ny});
+                    map[nx][ny] = map[x][y] + 1;
                 }
             }
         }
-
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        n =  Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        map = new int[n][m];
-        visited = new boolean[n][m];
-        distance = new int[n][m];
+        map = new int[n+1][m+1];
+        visited = new boolean[n+1][m+1];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             String s = br.readLine();
-            for (int j = 0; j < m; j++) {
-                int check = s.charAt(j) -'0';
-                map[i][j] = check;
+            for (int j = 1; j <= m; j++) {
+                map[i][j] = s.charAt(j-1) - '0';
             }
         }
 
-        bfs(0, 0);
+        bfs(1,1);
+        System.out.println(map[n][m]);
 
-        System.out.println(distance[n-1][m-1]);
     }
 }
