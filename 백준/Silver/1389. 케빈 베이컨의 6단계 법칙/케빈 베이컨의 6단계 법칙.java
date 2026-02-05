547b1d9f3c0fdb3d38e239ae717bb5db59a015fe
GYEONGNAM <feels19@naver.com>
2026-02-05 14:14:29 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 112 ms, Memory: 14656 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -3,74 +3,75 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static List<Integer>[] adj;
+    static int n, m;
+    static ArrayList<Integer>[] adj;
     static boolean[] visited;
-    static int N,M;
-    static int[] dist;
-    static int[] kevin;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        adj = new ArrayList[N+1];
-        visited = new boolean[N+1];
-        kevin = new int[N+1];
-
-        for (int i = 1; i <= N; i++) {
-            adj[i] = new ArrayList<>();
-        }
-
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-
-            adj[x].add(y);
-            adj[y].add(x);
-        }
-
-        int min = 1;
-        for (int i = 1; i <= N; i++) {
-            visited = new boolean[N + 1];
-            bfs(i);
-            if (kevin[i] < kevin[min]) {
-                min = i;
-            }
-        }
-
-        System.out.println(min);
-    }
-
-    static void bfs(int start) {
+    private static int bfs(int k) {
         Queue<Integer> q = new LinkedList<>();
-        q.add(start);
-        visited[start] = true;
-        dist = new int[N + 1];
+        int[] dist = new int[n+1];
+        visited = new boolean[n+1];
+
+        q.offer(k);
+        visited[k] = true;
 
         while (!q.isEmpty()) {
-            Integer cur = q.poll();
-
-            for (int next : adj[cur]) {
-                if (!visited[next]) {
-                    q.offer(next);
-                    visited[next] = true;
-                    dist[next] = dist[cur] + 1;
+            int now = q.poll();
+            for (int i : adj[now]) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    q.offer(i);
+                    dist[i] += dist[now] + 1;
                 }
             }
         }
 
-        for (int i = 1; i <= N; i++) {
-            kevin[start] += dist[i];
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += dist[i];
         }
+
+        return sum;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        adj = new ArrayList[n+1];
+
+        for (int i = 1; i <= n; i++) {
+            adj[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            adj[a].add(b);
+            adj[b].add(a);
+        }
+
+        int min = Integer.MAX_VALUE;
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int sum = bfs(i);
+            if (sum < min) {
+                min = sum;
+                answer = i;
+            }
+        }
+
+        System.out.println(answer);
     }
 }
