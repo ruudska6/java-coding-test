b7884796d1754bb33d04b640d017c786cc95d950
GYEONGNAM <feels19@naver.com>
2026-02-19 14:59:36 +0900

    [Silver III] Title: N과 M (4), Time: 208 ms, Memory: 20044 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -2,30 +2,38 @@ import java.util.Scanner;
 
 public class Main {
 
-    static int[] arr;
-    static int N,M;
+    static int N, M;
+    static int[] A;
+    static StringBuilder sb;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        N = scanner.nextInt();
-        M = scanner.nextInt();
-
-        arr = new int[M];
-
-        dfs(0, 1);
-    }
-
-    private static void dfs(int k, int start) {
-        if (k == M) {
+    static void dfs(int k, int level) {
+        if (level == M) {
             for (int i = 0; i < M; i++) {
-                System.out.print(arr[i] + " ");
+                sb.append(A[i]).append(" ");
             }
-            System.out.println();
+
+            sb.append("\n");
         } else {
-            for (int i = start; i <= N; i++) {
-                arr[k] = i;
-                dfs(k+1, i);
+
+            for (int i = 1; i <= N; i++) {
+                if (k <= i) {
+                    A[level] = i;
+                    dfs(i, level + 1);
+                }
             }
         }
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        sb = new StringBuilder();
+
+        N = scanner.nextInt();
+        M = scanner.nextInt();
+        A = new int[M];
+
+        dfs(1, 0);
+
+        System.out.println(sb);
     }
 }
