456fde7de6b517cc029251e1addb56746afde875
GYEONGNAM <feels19@naver.com>
2026-03-11 17:04:36 +0900

    [level 2] Title: 귤 고르기, Time: 75.61 ms, Memory: 97.3 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -6,20 +6,28 @@ class Solution {
         
         Map<Integer, Integer> map = new HashMap<>();
         
-        for (int x : tangerine) {
-            map.put(x, map.getOrDefault(x, 0) + 1);
+        for (int i = 0; i < tangerine.length; i++) {
+            map.put(tangerine[i], map.getOrDefault(tangerine[i], 0) + 1);
         }
         
+        // map을 key값을 기준으로 내림차순
         List<Integer> counts = new ArrayList<>(map.values());
         counts.sort(Collections.reverseOrder());
         
-        int sum = 0;
-        for (int c : counts) {
-            sum += c;
-            answer++;
-            if (sum >= k) break;
-        }
+        // 바구니에 갯수가 가장 많은 크기를 다넣고 => 이어서 다넣고 해서 k만큼을 채워야함
         
+        int count = 0;
+        for (int i = 0; i < counts.size(); i++) {
+            
+            count += counts.get(i);
+            
+            answer++;
+            
+            if (count >= k) {
+                break;
+            }
+        }
+    
         return answer;
     }
 }
