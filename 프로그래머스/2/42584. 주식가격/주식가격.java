9a4f998041c99f9293f103b429d44e4cc1149e03
GYEONGNAM <feels19@naver.com>
2026-03-11 16:50:25 +0900

    [level 2] Title: 주식가격, Time: 16.37 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] solution(int[] prices) {
+        
+        int[] answer = new int[prices.length];
+        
+        
+        for (int i = 0; i < prices.length - 1; i++) {
+        
+            int seconds = 0;
+            
+            
+            for (int j = i + 1; j < prices.length; j++) {
+                seconds++;
+                
+                if (prices[i] > prices[j]) break;
+            } 
+            
+            answer[i] = seconds;   
+        }
+        
+        return answer;
+    }
+}
