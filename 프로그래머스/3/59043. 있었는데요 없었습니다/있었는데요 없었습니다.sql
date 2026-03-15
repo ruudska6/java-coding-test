78f74e345c35d23cd065aca33643975bd88841a1
GYEONGNAM <feels19@naver.com>
2026-03-15 20:02:18 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -0,0 +1,14 @@
+-- 코드를 입력하세요
+SELECT
+    ins.animal_id, ins.name
+FROM 
+    animal_ins ins
+JOIN
+    animal_outs outs
+ON
+    ins.animal_id = outs.animal_id
+
+WHERE
+    ins.datetime > outs.datetime
+ORDER BY
+    ins.datetime;
