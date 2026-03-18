75236f6b97ba3ab7fc7aa218cd154bf2491a28b2
GYEONGNAM <feels19@naver.com>
2026-03-18 12:47:12 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 95.51 ms, Memory: 98.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < participant.length; i++) {
+            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
+        }
+        
+        for (int i = 0; i < completion.length; i++) {
+            if (map.get(completion[i]) > 0) {
+                map.put(completion[i], map.get(completion[i]) - 1);
+            }
+            
+            if (map.get(completion[i]) <= 0) {
+                map.remove(completion[i]);
+            }
+        }
+        
+        for (String x : map.keySet()) {
+            answer = x;
+        }
+        
+        return answer;
+    }
+}
