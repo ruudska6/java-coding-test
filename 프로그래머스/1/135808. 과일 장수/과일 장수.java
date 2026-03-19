e85445f5eec7c26c1e2011a84c5976df349cf584
GYEONGNAM <feels19@naver.com>
2026-03-19 13:16:52 +0900

    [level 1] Title: 과일 장수, Time: 63.48 ms, Memory: 150 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -3,25 +3,15 @@ import java.util.*;
 class Solution {
     public int solution(int k, int m, int[] score) {
         
-        // score 내림차순 정렬
-        score = change(score);
+        // score 정렬
         Arrays.sort(score);
-        score = change(score);
     
         // 높은 점수부터 박스에 담음
         int sum = 0;
-        for (int i = m - 1; i < score.length; i += m) {
+        for (int i = score.length - m; i >= 0; i -= m) {
             sum += score[i] * m;
         }
     
         return sum;
-    }
-    
-    public static int[] change(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = arr[i] * -1;
-        }
-        
-        return arr;
     }
 }
