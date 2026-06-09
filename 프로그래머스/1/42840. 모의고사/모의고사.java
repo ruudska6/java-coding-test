70f50f36fb112cabca7c61f973fd3bece3805188
GYEONGNAM <feels19@naver.com>
2026-06-09 20:24:57 +0900

    [level 1] Title: 모의고사, Time: 0.43 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,39 +2,34 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        
-        int[] scores = new int[3];
-        
-        int[] one = new int[] {1, 2, 3, 4, 5};
-        int[] two = new int[] {2, 1, 2, 3, 2, 4, 2 ,5};
-        int[] three = new int[] {3, 3, 1, 1, 2, 2, 4, 4, 5,5};
-        
+    
+        int[] one = {1, 2, 3, 4, 5};
+        int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+                
+        int oneCnt = 0;
+        int twoCnt = 0;
+        int threeCnt = 0;
         for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == one[i % one.length]) scores[0]++;
-            if (answers[i] == two[i % two.length]) scores[1]++;
-            if (answers[i] == three[i % three.length]) scores[2]++;
+            if (one[i % one.length] == answers[i]) oneCnt++;
+            if (two[i % two.length] == answers[i]) twoCnt++;
+            if (three[i % three.length] == answers[i]) threeCnt++;
         }
         
-        int max = 0;
-        for (int i = 0; i < scores.length; i++) {
-            max = Math.max(max, scores[i]);
-        }
+        int max = Math.max(oneCnt, twoCnt);
+        max = Math.max(max, threeCnt);
+           
+        List<Integer> rank = new ArrayList<>();
+        if (oneCnt == max) rank.add(1);
+        if (twoCnt == max) rank.add(2);
+        if (threeCnt == max) rank.add(3);
         
-        List<Integer> list = new ArrayList<>();
-        
-        for (int i = 0; i < scores.length; i++) {
-            if (scores[i] == max) list.add(i+1);
-        }
-        
-        
-        int[] answer = new int[list.size()];
-        
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
-        
-        Arrays.sort(answer);
-        
+        int[] answer = new int[rank.size()];
+
+        for (int i = 0; i < rank.size(); i++) {
+            answer[i] = rank.get(i);
+        }    
+    
         return answer;
     }
 }
