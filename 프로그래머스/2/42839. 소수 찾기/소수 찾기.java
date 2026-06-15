b13789bfc1bc4818e87d5266c2b48f9410b4df96
GYEONGNAM <feels19@naver.com>
2026-06-15 19:45:33 +0900

    [level 2] Title: 소수 찾기, Time: 10.14 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -4,14 +4,14 @@ class Solution {
     static Set<Integer> set;
     static boolean[] visited;
     
-    private void dfs(String k, String numbers) {
+    private static void dfs(String k, String numbers) {
         if (!k.isEmpty()) {
             set.add(Integer.parseInt(k));
         }
         
         if (k.length() == numbers.length()) return;
-
-        for (int i = 0; i < numbers.length(); i++) { 
+        
+        for (int i = 0; i < numbers.length(); i++) {
             if (!visited[i]) {
                 visited[i] = true;
                 dfs(k + numbers.charAt(i), numbers);
@@ -20,11 +20,11 @@ class Solution {
         }
     }
     
-    private static boolean isPrime(int n) {
-        if (n < 2) return false;
-
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) return false;
+    private static boolean isPrime(int number) {
+        if (number < 2) return false;
+        
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) return false;
         }
         
         return true;
@@ -33,6 +33,7 @@ class Solution {
     public int solution(String numbers) {
         int answer = 0;
         
+        List<Integer> nums = new ArrayList<>();
         set = new HashSet<>();
         visited = new boolean[numbers.length()];
         
@@ -44,4 +45,6 @@ class Solution {
         
         return answer;
     }
+    
+    
 }
