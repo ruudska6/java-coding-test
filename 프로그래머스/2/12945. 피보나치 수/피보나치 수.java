f76195f3e9c35055bc144d6307225a7936c75e95
GYEONGNAM <feels19@naver.com>
2026-06-28 17:14:23 +0900

    [level 2] Title: 피보나치 수, Time: 1.65 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -5,18 +5,18 @@ class Solution {
     }
 
     int fibonacci(int n) {
-        final int MOD = 1234567;
         if (n == 0) return 0;
         if (n == 1) return 1;
         if (n == 2) return 1;
 
         int[] dp = new int[n + 1];
+
         dp[0] = 0;
         dp[1] = 1;
         dp[2] = 1;
 
         for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD; // 매 단계에서 모듈러
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
         }
 
         return dp[n];
