3870bab62926f3de115642eb6284b630f05609e9
GYEONGNAM <feels19@naver.com>
2026-07-01 20:19:33 +0900

    [level 2] Title: 피로도, Time: 1.53 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?gad_source=1&gad_campaignid=23716289893&gbraid=0AAAAAC_c4nCvA_SmkVJ8Hreq2noKXCVc-&gclid=Cj0KCQjw9ZLSBhCcARIsAEhGKgP3tWcjjhoToOCJrKUDEf2Qsb8jsQPJuDPZSfMIxrjQ6FpwRHJNxcIaAvRvEALw_wcB) 
 
 ### 성능 요약
 
-메모리: 90.2 MB, 시간: 3.09 ms
+메모리: 74.1 MB, 시간: 1.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 01일 13:07:58
+2026년 07월 01일 20:19:28
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,26 +1,25 @@
 class Solution {
-    static boolean[] visited; 
-    static int maxCount;
+    
+    static int max = Integer.MIN_VALUE;
+    static boolean[] visited;
+    
+    private static void dfs(int depth, int hp, int[][]dungeons) { 
+        
+        max = Math.max(max, depth);
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!visited[i] && dungeons[i][0] <= hp) {
+                visited[i] = true;
+                dfs(depth + 1, hp - dungeons[i][1], dungeons);
+                visited[i] = false;
+            }
+        }
+    }
     
     public int solution(int k, int[][] dungeons) {
+        int answer = -1;
         visited = new boolean[dungeons.length];
-        
-        dfs(k, 0, dungeons);
-        
-        return maxCount;
-        
-
-    }
-
-    private static void dfs(int k, int depth, int[][] dungeons) {
-        maxCount = Math.max(maxCount, depth);
-           
-        for (int i = 0; i < dungeons.length; i++) {
-            if (!visited[i] && k >= dungeons[i][0]) {
-                    visited[i] = true;
-                    dfs(k - dungeons[i][1], depth + 1, dungeons);
-                    visited[i] = false;
-            } 
-        }    
+        dfs(0, k, dungeons);
+    
+        return max == Integer.MIN_VALUE ? answer : max;    
     }
 }
