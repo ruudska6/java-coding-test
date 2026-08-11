918550bebad7df7d97ba73940c9235b32fe8e931
GYEONGNAM <feels19@naver.com>
2026-08-11 16:19:28 +0900

    [level 2] Title: 소수 찾기, Time: 10.22 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,50 +1,49 @@
 import java.util.*;
 
 class Solution {
-    static Set<Integer> set;
+    static String[] arr;
     static boolean[] visited;
-    
-    private static void dfs(String k, String numbers) {
-        if (!k.isEmpty()) {
-            set.add(Integer.parseInt(k));
-        }
+    static Set<Integer> nums = new HashSet<>();
+    public int solution(String numbers) {
+        int answer = 0;
+        arr = new String[numbers.length()];
+        visited = new boolean[numbers.length()];
+        dfs("", numbers, 0);
         
-        if (k.length() == numbers.length()) return;
+        for (int num : nums) {
+            if (isPrime(num)) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+    
+    private void dfs(String now, String numbers, int depth) {
+        if (depth == numbers.length()) {
+            return;
+        }
         
         for (int i = 0; i < numbers.length(); i++) {
             if (!visited[i]) {
-                visited[i] = true;
-                dfs(k + numbers.charAt(i), numbers);
+                visited[i] = true;                
+                nums.add(Integer.parseInt(now + numbers.charAt(i)));
+                dfs(now + numbers.charAt(i), numbers, depth + 1);    
                 visited[i] = false;
-            }
+            } 
+       
         }
     }
     
     private static boolean isPrime(int number) {
+        
         if (number < 2) return false;
         
         for (int i = 2; i <= Math.sqrt(number); i++) {
-            if (number % i == 0) return false;
+            if (number % i == 0) {
+                return false;
+            } 
         }
         
         return true;
     }
-    
-    public int solution(String numbers) {
-        int answer = 0;
-        
-        List<Integer> nums = new ArrayList<>();
-        set = new HashSet<>();
-        visited = new boolean[numbers.length()];
-        
-        dfs("", numbers);
-        
-        for (int x : set) {
-            if (isPrime(x)) answer++;
-        }
-        
-        return answer;
-    }
-    
-    
 }
