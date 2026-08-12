9fce5975fa1e549e8535dd43d53430d00b7a13f7
GYEONGNAM <feels19@naver.com>
2026-08-12 14:27:46 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 59.89 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,26 +4,27 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         
+        // 이름, 명수를 map에 저장
         Map<String, Integer> map = new HashMap<>();
         
+        // map에 등록
         for (int i = 0; i < participant.length; i++) {
-            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
+            map.put(participant[i], 
+                    map.getOrDefault(participant[i], 0) + 1);
         }
         
-        for (int i = 0; i < completion.length; i++) {
-            if (map.get(completion[i]) > 0) {
-                map.put(completion[i], map.get(completion[i]) - 1);
-            }
-            
-            if (map.get(completion[i]) <= 0) {
-                map.remove(completion[i]);
-            }
-        }
+        // 완주자 명단 순회하면서 완주자 제거
+        for (String name : completion) {
+            // 동명이인 제거 
+            map.put(name, map.get(name) - 1);
+            if (map.get(name) == 0) map.remove(name);
+        }        
         
-        for (String x : map.keySet()) {
-            answer = x;
+        // map에 남아있는 사람 출력
+        for (String name : map.keySet()) {
+            answer = name;
         }
-        
+    
         return answer;
     }
 }
