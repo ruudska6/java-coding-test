a7b6383bd05f8ee003bebca3a852e23c928ed552
GYEONGNAM <feels19@naver.com>
2026-08-13 15:31:21 +0900

    [level 2] Title: 전화번호 목록, Time: 190.39 ms, Memory: 234 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,31 +2,19 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        
-        boolean answer = true;
-        
-        Map<String, Integer> list = new HashMap<>();
-        
+            
+        Set<String> set = new HashSet<>();
         for (int i = 0; i < phone_book.length; i++) {
-            list.put(phone_book[i], 1);
+           set.add(phone_book[i]);
         }
         
         for (int i = 0; i < phone_book.length; i++) {
-            String target = phone_book[i];
-            
-            for (int j = 1; j < target.length(); j++) {
-                String prefix = target.substring(0, j);
-                
-                if (list.containsKey(prefix)) {
-                    answer = false;
-                }
+            for (int j = 1; j < phone_book[i].length(); j++) {
+                String prefix = phone_book[i].substring(0, j);
+                if (set.contains(prefix)) return false;
             }
-            
         }
         
-        
-        
-        
-        return answer;
+        return true;
     }
 }
