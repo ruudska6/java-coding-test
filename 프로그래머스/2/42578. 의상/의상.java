ac1c86e984b271fe0e01f873f72057ae309e6d4c
GYEONGNAM <feels19@naver.com>
2026-08-13 18:31:14 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,20 +2,18 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
         
-        Map<String, Integer> inventory = new HashMap<>(); 
-        
+        Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < clothes.length; i++) {
-            inventory.put(clothes[i][1], inventory.getOrDefault(clothes[i][1], 0) + 1);
+           String kind = clothes[i][1];
+            map.put(kind, map.getOrDefault(kind, 0) + 1); 
         }
         
-        
-        for (String x : inventory.keySet()) {
-            answer *= inventory.get(x) + 1;
+        int sum = 1;
+        for (String kind : map.keySet()) {
+            sum *= map.get(kind) + 1;
         }
-       
-        return answer - 1;
-        
+
+        return sum - 1;
     }
 }
