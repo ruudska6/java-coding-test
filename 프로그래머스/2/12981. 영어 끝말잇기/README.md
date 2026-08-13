af3e2c04c232524493001d02abba811b778b88b3
GYEONGNAM <feels19@naver.com>
2026-08-13 16:43:26 +0900

    [level 2] Title: 영어 끝말잇기, Time: 1.25 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/README.md
+++ 프로그래머스/2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981?gad_source=1&gad_campaignid=22356298761&gbraid=0AAAAAC_c4nCygzTuG1P3NEP-SrUOeHHdB&gclid=CjwKCAjws_DTBhB_EiwAXZknGZYwnkNlBiDNWra7eVfht9v2iz8TxpwQpqbBapCT8uXJrvwIb6kyBRoC_8YQAvD_BwE) 
 
 ### 성능 요약
 
-메모리: 72.2 MB, 시간: 0.10 ms
+메모리: 86.5 MB, 시간: 1.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 18일 21:46:56
+2026년 08월 13일 16:43:23
 
 ### 문제 설명
 
--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -2,30 +2,40 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        int[] answer = new int[2];
         
-        Map<String, Boolean> check = new HashMap<>();
+        // 단어를 저장할 Set
+        Set<String> set = new HashSet<>();
         
-        char lastWord = words[0].charAt(words[0].length() - 1);
-        check.put(words[0], true);
+        // 몇번 참가자가 탈락했는지 저장
+        int turn = 2;
+        int round = 2;
+        char last = words[0].charAt(words[0].length() - 1);
+        set.add(words[0]);
         
-        for (int i = 1; i < words.length; i++) {
-            if (check.containsKey(words[i])) {
-                answer[0] = (i % n) + 1; 
-                answer[1] = (i / n) + 1;
-                return answer;
-            }
-            check.put(words[i], true);
-            
-            if(words[i].charAt(0) != lastWord) {
-                answer[0] = (i % n) + 1; 
-                answer[1] = (i / n) + 1;
-                return answer;
+        for (int i = 1; i < words.length; i++) {           
+            // 첫 단어가 last와 같은지 확인
+            char first = words[i].charAt(0);
+                        
+            if (first != last) {
+                return new int[] {turn, (round + n - 1) / n};
             }
             
-            lastWord = words[i].charAt(words[i].length() - 1);
+            last = words[i].charAt(words[i].length() - 1);
+
+            // 중복인지 확인
+            if (set.contains(words[i])) {
+                return new int[] {turn, (round + n - 1) / n};
+            }
+            
+            // 단어 저장
+            set.add(words[i]);
+            
+            turn = (turn % n) + 1;
+            round++;
+
+            System.out.println(words[i]);
         }
         
-        return new int[]{0, 0};
+        return new int[] {0, 0};
     }
 }
